851bc0c5eb4a24897dd18db3eb402267e8b2538d
Samuel Kyletoft <samuel@kyletoft.se>
2023-09-06 12:36:38 +0200

    Initial commit

---
+++ flake.nix
@@ -0,0 +1,23 @@
+{
+	description = "A very basic flake";
+
+	inputs = {
+		nixpkgs.url     = "github:nixos/nixpkgs/nixpkgs-unstable";
+		flake-utils.url = "github:numtide/flake-utils";
+	};
+
+	outputs = { self, nixpkgs, flake-utils }:
+		flake-utils.lib.eachDefaultSystem(system:
+			let
+				pkgs = nixpkgs.legacyPackages.${system};
+				lib = nixpkgs.lib;
+			in rec {
+				devShells.default = pkgs.mkShell {
+					nativeBuildInputs = with pkgs; [
+						idris2
+						gnumake
+					];
+				};
+			}
+		);
+}
